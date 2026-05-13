8275422daa02bc08473e0a56b0cfc7c2c6fd1f4f
Soap <artyom.krivoxin@mail.ru>
2026-05-13 23:10:45 +0300

    feat: add init file for api

---
+++ app/api/__init__.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+
+from app.api.auth.router import router as auth_router
+from app.api.restaurants.router import router as restaurants_router
+from app.api.dishes.router import router as dishes_router
+from app.api.orders.router import router as orders_router
+from app.api.orders.tracking import router as tracking_router
+from app.api.favorites.router import router as favorites_router
+from app.api.admin.router import router as admin_router
+
+api_router = APIRouter()
+
+# Подключаем все роутеры
+api_router.include_router(auth_router)
+api_router.include_router(restaurants_router)
+api_router.include_router(dishes_router)
+api_router.include_router(orders_router)
+api_router.include_router(tracking_router)
+api_router.include_router(favorites_router)
+api_router.include_router(admin_router)
